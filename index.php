b4727da3d23b13c42f4ba6fde37bb5b259084f90
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2016-07-20 14:19:56 -0500

    Como usar Birt(java)
    - Se crear un reporte en PDF con birt
    - Se embebe todo el contenido de birt en iframe

--- index.php
+++ index.php
@@ -8,9 +8,46 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>PHP + Java(Birt)</title>
+        <script type="text/javascript" src="js/pdfobject.min.js"></script>
+        <style type="text/css">
+            .pdfobject-container{
+                -moz-box-shadow: 7px 6px 5px #6b696b;
+                -webkit-box-shadow: 7px 6px 5px #6b696b;
+                box-shadow: 7px 6px 5px #6b696b;
+                height: 500px;
+                width: 100%;
+            }
+            .pdfobject{
+                border: 1px solid #666;
+            }
+            
+            #reporte{
+                width: 100%;
+                height: 500px;
+            }
+        </style>
     </head>
     <body>
-        <h1>Como embeber un reporte con Birt(Java) en PHP</h1>
+        <center>
+            <h1>Como embeber un reporte con Birt(Java) en PHP</h1>
+        </center>
+        <?php
+//            $fp = fopen("http://localhost:8080/birt/frameset?__report=test.rptdesign&sample=my+parameter", "r");
+//            while(!feof($fp)) {
+//                $linea = fgets($fp);
+//                echo $linea;
+//            }
+//            fclose($fp); 
+        ?>
         
+        <h3>Reporte en PDF con birt</h3>
+        <div id="showReport" ></div>
+        <script type="text/javascript">
+            PDFObject.embed("http://localhost:8080/birt/frameset?__report=test.rptdesign&sample=my+parameter&__format=pdf", "#showReport");
+        </script>
+        <h3>Como embeber Birt en un iframe</h3>
+        <FRAMESET rows="*">
+            <iframe name="central" src="http://localhost:8080/birt/frameset?__report=test.rptdesign&sample=my+parameter" id="reporte">
+        </FRAMESET>
     </body>
 </html>
