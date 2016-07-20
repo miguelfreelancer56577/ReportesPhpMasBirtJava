fd9741ae1c6ea493cafd03d48b1024babfc31fc7
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2016-07-20 13:12:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP + Java(Birt)</title>
+    </head>
+    <body>
+        <h1>Como embeber un reporte con Birt(Java) en PHP</h1>
+        
+    </body>
+</html>
